7adb2d76d2a6c014d302e336da5bbe9b22385779
sagar-nigam <sagar.nigam.gr@gmail.com>
2023-04-01 21:30:50 +0200

    navigation bar view complete

--- components/bottom-navigation.tsx
+++ components/bottom-navigation.tsx
@@ -1,9 +1,45 @@
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Image, StyleSheet, TouchableOpacity, View} from 'react-native';
 
 const BottomNavigation = (): JSX.Element => {
+  const [activeScreen, setActiveScreen] = useState('home');
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello, world!</Text>
+      <TouchableOpacity
+        style={styles.logoContainer}
+        onPress={() => setActiveScreen('home')}>
+        <Image style={styles.tinyLogo} source={require('../assets/home.png')} />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.logoContainer}
+        onPress={() => setActiveScreen('workout')}>
+        <Image
+          style={styles.tinyLogo}
+          source={require('../assets/excercise.png')}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.logoContainer}
+        onPress={() => setActiveScreen('add')}>
+        <Image
+          style={styles.tinyLogo}
+          source={require('../assets/button.png')}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.logoContainer}
+        onPress={() => setActiveScreen('groceries')}>
+        <Image
+          style={styles.tinyLogo}
+          source={require('../assets/groceries.png')}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.logoContainer}
+        onPress={() => setActiveScreen('news')}>
+        <Image style={styles.tinyLogo} source={require('../assets/news.png')} />
+      </TouchableOpacity>
     </View>
   );
 };
@@ -11,14 +47,18 @@ const BottomNavigation = (): JSX.Element => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red',
+    backgroundColor: 'transparent',
     justifyContent: 'flex-end',
     alignItems: 'center',
+    flexDirection: 'row',
   },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#333',
+  logoContainer: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  tinyLogo: {
+    width: 32,
+    height: 32,
   },
 });
 

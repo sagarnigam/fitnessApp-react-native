a5e133dedb83b51b9645fb4aa4117e96bc293832
sagar-nigam <sagar.nigam.gr@gmail.com>
2023-04-03 11:56:31 +0200

    components added

---
+++ screens/home.tsx
@@ -0,0 +1,25 @@
+import {StyleSheet, Text, View} from 'react-native';
+
+const Home = (): JSX.Element => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello to home</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+});
+
+export default Home;

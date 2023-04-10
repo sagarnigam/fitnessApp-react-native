52c54e9a201862a0ab544cc67afd64d567f075b6
sagar-nigam <sagar.nigam.gr@gmail.com>
2023-04-10 11:31:41 +0200

    exercise data added

---
+++ mock-data/exercise-data.ts
@@ -0,0 +1,74 @@
+export const EXERCISES = {
+  chest: [
+    {
+      id: '1',
+      name: 'Chest Press',
+    },
+    {
+      id: '2',
+      name: 'Incline Chest Press',
+    },
+    {
+      id: '3',
+      name: 'Cable Chest Press',
+    },
+    {
+      id: '4',
+      name: 'Chest Fly',
+    },
+  ],
+  back: [
+    {
+      id: '5',
+      name: 'Pull Ups',
+    },
+    {
+      id: '6',
+      name: 'Pull Down',
+    },
+    {
+      id: '7',
+      name: 'Dumbell Rows',
+    },
+    {
+      id: '8',
+      name: 'Cable Rows',
+    },
+  ],
+  legs: [
+    {
+      id: '9',
+      name: 'Barbell Squats',
+    },
+    {
+      id: '10',
+      name: 'Hip Thrust',
+    },
+    {
+      id: '11',
+      name: 'Dumbell Walking Lunges',
+    },
+    {
+      id: '12',
+      name: 'Barbell Deadlift',
+    },
+  ],
+  biceps: [
+    {
+      id: '13',
+      name: 'Dumbell Bicep Curl',
+    },
+  ],
+  triceps: [
+    {
+      id: '14',
+      name: 'Dumbell Overhaed Tricep',
+    },
+  ],
+  hiit: [
+    {
+      id: '15',
+      name: 'Jump Rope',
+    },
+  ],
+};

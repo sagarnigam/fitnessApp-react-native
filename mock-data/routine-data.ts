e4e655cec4cabb9436abd5c07d4dd36c5806cf43
sagar-nigam <sagar.nigam.gr@gmail.com>
2023-04-17 16:44:02 +0200

    data added

--- mock-data/routine-data.ts
+++ mock-data/routine-data.ts
@@ -8,38 +8,6 @@ export const ROUTINES: any[] = [
     exercise: {
       'Day 1': [
         {
-          name: 'Chest Press',
-          description: '2 sets, 20 Reps, 1 min rest',
-          type: 'normal',
-        },
-        {
-          name: 'DB Incline chest Press',
-          description: '2 sets, 20 Reps, 1 min rest',
-          type: 'normal',
-        },
-        {
-          name: 'Cable Chest Fly',
-          description: '2 sets, 20 Reps, 1 min rest',
-          type: 'normal',
-        },
-        {
-          name: 'Triceps Pushdown',
-          description: '2 sets, 20 Reps, 1 min rest',
-          type: 'normal',
-        },
-        {
-          name: 'Skull Crusher',
-          description: '2 sets, 20 Reps, 1 min rest',
-          type: 'normal',
-        },
-        {
-          name: 'Pull ups',
-          description: '2 sets, 20 Reps, 1 min rest',
-          type: 'normal',
-        },
-      ],
-      'Day 2': [
-        {
           name: 'Barbell Hip thrust',
           type: 'normal',
           description: '2 sets, 20 Reps, 1 min rest',
@@ -95,10 +63,198 @@ export const ROUTINES: any[] = [
           ],
         },
       ],
+      'Day 2': [
+        {
+          name: 'Rest Day',
+          type: 'normal',
+          description: 'Rest Day with light stretching',
+        },
+      ],
       'Day 3': [],
-      'Day 4': [],
+      'Day 4': [
+        {
+          name: 'Rest Day',
+          type: 'normal',
+          description: 'Rest Day with light stretching',
+        },
+      ],
       'Day 5': [],
       'Day 6': [],
+      'Day 7': [
+        {
+          name: 'Rest Day',
+          type: 'normal',
+          description: 'Rest Day with light stretching',
+        },
+      ],
+      'Day 8': [],
+      'Day 9': [
+        {
+          name: 'Rest Day',
+          type: 'normal',
+          description: 'Rest Day with light stretching',
+        },
+      ],
+      'Day 10': [],
+      'Day 11': [
+        {
+          name: 'Rest Day',
+          type: 'normal',
+          description: 'Rest Day with light stretching',
+        },
+      ],
+      'Day 12': [],
+      'Day 13': [],
+      'Day 14': [
+        {
+          name: 'Rest Day',
+          type: 'normal',
+          description: 'Rest Day with light stretching',
+        },
+      ],
+      'Day 15': [],
+      'Day 16': [
+        {
+          name: 'Rest Day',
+          type: 'normal',
+          description: 'Rest Day with light stretching',
+        },
+      ],
+      'Day 17': [],
+      'Day 18': [
+        {
+          name: 'Rest Day',
+          type: 'normal',
+          description: 'Rest Day with light stretching',
+        },
+      ],
+      'Day 19': [],
+      'Day 20': [],
+      'Day 21': [
+        {
+          name: 'Rest Day',
+          type: 'normal',
+          description: 'Rest Day with light stretching',
+        },
+      ],
+      'Day 22': [],
+      'Day 23': [
+        {
+          name: 'Rest Day',
+          type: 'normal',
+          description: 'Rest Day with light stretching',
+        },
+      ],
+      'Day 24': [],
+      'Day 25': [
+        {
+          name: 'Rest Day',
+          type: 'normal',
+          description: 'Rest Day with light stretching',
+        },
+      ],
+      'Day 26': [],
+      'Day 27': [],
+      'Day 28': [
+        {
+          name: 'Rest Day',
+          type: 'normal',
+          description: 'Rest Day with light stretching',
+        },
+      ],
+      'Day 29': [],
+      'Day 30': [
+        {
+          name: 'Rest Day',
+          type: 'normal',
+          description: 'Rest Day with light stretching',
+        },
+      ],
+      'Day 31': [],
+      'Day 32': [
+        {
+          name: 'Rest Day',
+          type: 'normal',
+          description: 'Rest Day with light stretching',
+        },
+      ],
+      'Day 33': [],
+      'Day 34': [],
+      'Day 35': [
+        {
+          name: 'Rest Day',
+          type: 'normal',
+          description: 'Rest Day with light stretching',
+        },
+      ],
+      'Day 36': [],
+      'Day 37': [
+        {
+          name: 'Rest Day',
+          type: 'normal',
+          description: 'Rest Day with light stretching',
+        },
+      ],
+      'Day 38': [],
+      'Day 39': [
+        {
+          name: 'Rest Day',
+          type: 'normal',
+          description: 'Rest Day with light stretching',
+        },
+      ],
+      'Day 40': [],
+      'Day 41': [],
+      'Day 42': [
+        {
+          name: 'Rest Day',
+          type: 'normal',
+          description: 'Rest Day with light stretching',
+        },
+      ],
+      'Day 43': [],
+      'Day 44': [
+        {
+          name: 'Rest Day',
+          type: 'normal',
+          description: 'Rest Day with light stretching',
+        },
+      ],
+      'Day 45': [],
+      'Day 46': [
+        {
+          name: 'Rest Day',
+          type: 'normal',
+          description: 'Rest Day with light stretching',
+        },
+      ],
+      'Day 47': [],
+      'Day 48': [],
+      'Day 49': [
+        {
+          name: 'Rest Day',
+          type: 'normal',
+          description: 'Rest Day with light stretching',
+        },
+      ],
+      'Day 50': [],
+      'Day 51': [
+        {
+          name: 'Rest Day',
+          type: 'normal',
+          description: 'Rest Day with light stretching',
+        },
+      ],
+      'Day 52': [],
+      'Day 53': [
+        {
+          name: 'Rest Day',
+          type: 'normal',
+          description: 'Rest Day with light stretching',
+        },
+      ],
+      'Day 54': [],
+      'Day 55': [],
     },
     estTime: '',
   },

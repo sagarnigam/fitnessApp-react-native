5c915147719f821785f292ead606b7fd0ad575a7
sagar-nigam <sagar.nigam.gr@gmail.com>
2023-05-01 13:42:21 +0200

    list added and buttons

--- components/exercise-home.tsx
+++ components/exercise-home.tsx
@@ -1,26 +1,82 @@
 import React from 'react';
-import {FlatList, StyleSheet, Text, View, Image} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Animated,
+  Button,
+  Pressable,
+} from 'react-native';
 import {BODY_PARTS} from '../mock-data/exercise-data';
 
 const gluteIcon = require('../assets/glutes.png');
 
 const ExerciseHome = (): JSX.Element => {
+  const scrollY = React.useRef(new Animated.Value(0)).current;
+  const ITEMSIZE = 84;
+
+  const onPress = () => {
+    console.log('button Pressed');
+  };
+
   return (
     <View style={styles.container}>
-      <FlatList
-        data={BODY_PARTS}
-        keyExtractor={item => item.key}
-        contentContainerStyle={{
-          padding: 8,
-        }}
-        renderItem={item => {
-          return (
-            <View style={styles.exercisePill}>
-              <Text style={styles.text}>{item.item.name}</Text>
-            </View>
-          );
-        }}
-      />
+      <View style={styles.listContainer}>
+        <Animated.FlatList
+          data={BODY_PARTS}
+          keyExtractor={item => item.key}
+          onScroll={Animated.event(
+            [{nativeEvent: {contentOffset: {y: scrollY}}}],
+            {useNativeDriver: true},
+          )}
+          contentContainerStyle={{
+            padding: 4,
+          }}
+          renderItem={({item, index}) => {
+            const inputRange = [
+              -1,
+              0,
+              ITEMSIZE * index,
+              ITEMSIZE * (index + 2),
+            ];
+            const opacityInputRange = [
+              -1,
+              0,
+              ITEMSIZE * index,
+              ITEMSIZE * (index + 1),
+            ];
+            const scale = scrollY.interpolate({
+              inputRange,
+              outputRange: [1, 1, 1, 0],
+            });
+            const opacity = scrollY.interpolate({
+              inputRange: opacityInputRange,
+              outputRange: [1, 1, 1, 0],
+            });
+            return (
+              <Animated.View
+                style={[
+                  styles.exercisePill,
+                  {transform: [{scale}], opacity: opacity},
+                ]}>
+                <Text style={styles.text}>Test </Text>
+              </Animated.View>
+            );
+          }}
+        />
+      </View>
+      <View style={styles.buttonsContainer}>
+        <Pressable
+          style={[styles.button, {backgroundColor: '#319AE5'}]}
+          onPress={onPress}>
+          <Text style={styles.buttonText}>My Routines</Text>
+        </Pressable>
+        <Pressable
+          style={[styles.button, {backgroundColor: '#E44040'}]}
+          onPress={onPress}>
+          <Text style={styles.buttonText}>Build Workout Routine</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
@@ -31,13 +87,40 @@ const styles = StyleSheet.create({
     paddingTop: 100,
     paddingBottom: 70,
     backgroundColor: '#09131F',
+    flexDirection: 'column',
   },
   text: {
     fontSize: 24,
     fontWeight: 'bold',
     color: 'white',
   },
-  exercisePill: {},
+  listContainer: {
+    flex: 5,
+    paddingBottom: 20,
+  },
+  buttonsContainer: {
+    flex: 1,
+    padding: 8,
+  },
+  exercisePill: {
+    backgroundColor: 'pink',
+    height: 70,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 15,
+    margin: 4,
+  },
+  button: {
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 15,
+    marginBottom: 6,
+  },
+  buttonText: {
+    fontSize: 16,
+    color: 'white',
+  },
 });
 
 export default ExerciseHome;
